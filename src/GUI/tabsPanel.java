8364640ce1494cbcc8c6b2ee93fb2507b1b5174e
mdoodewa <46785063+mdoodewa@users.noreply.github.com>
2019-01-25 15:12:36 +0100

    Databases created

--- src/GUI/tabsPanel.java
+++ src/GUI/tabsPanel.java
@@ -1,11 +1,18 @@
 package GUI;
 
+import com.sun.crypto.provider.JceKeyStore;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 
 public class tabsPanel extends JPanel {
+
+    private TextField text = new TextField(20);
+    private JFrame test = new JFrame();
 
     public tabsPanel(){
         super(new FlowLayout(FlowLayout.LEFT));
@@ -13,7 +20,7 @@ public class tabsPanel extends JPanel {
         setBackground(Color.red);
     }
 
-    public void createComponents(){
+    private void createComponents(){
         JButton Overzicht1 = new JButton("Overzicht1");
         Overzicht1.setPreferredSize(new Dimension(100,40));
         JButton Overzicht2 = new JButton("Overzicht2");
@@ -22,9 +29,23 @@ public class tabsPanel extends JPanel {
         Overzicht3.setPreferredSize(new Dimension(100,40));
         JButton Overzicht4 = new JButton("Overzicht4");
         Overzicht4.setPreferredSize(new Dimension(100,40));
-        add(Overzicht1);
+
+        //Overzicht1.addActionListener(this);
+        JComponent overzicht1 = makeNewPanel();
+        add(Overzicht1,overzicht1);
         add(Overzicht2);
         add(Overzicht3);
         add(Overzicht4);
+    }
+
+    //@Override
+    //public void actionPerformed(ActionEvent e) {
+    //    makeNewPanel();
+    //}
+
+    private JComponent makeNewPanel(){
+        JPanel newPanel = new JPanel();
+        newPanel.add(new JTextField("test"));
+        return newPanel;
     }
 }
